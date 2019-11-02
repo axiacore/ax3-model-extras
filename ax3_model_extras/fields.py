adfb162f3ca4b0818be7b7f35728a3915772e404
Camilo Nova <camilo.nova@axiacore.com>
2019-11-02 16:03:26 -0500

    Added webp generation by function

--- ax3_model_extras/fields.py
+++ ax3_model_extras/fields.py
@@ -1,19 +1,16 @@
 from io import BytesIO
-import os.path
-import subprocess
 
 from django.core.exceptions import ValidationError
-from django.core.files.base import ContentFile
 from django.db.models import ImageField
 
 from PIL import Image
 from resizeimage import resizeimage
 from resizeimage.imageexceptions import ImageSizeError
 
+from .webp import generate_webp
+
 
 class OptimizedImageField(ImageField):
-    _image_format = None
-
     def __init__(self, *args, **kwargs):
         """
         `optimized_image_output_size` must be a tuple and `optimized_image_resize_method` can be
@@ -28,7 +25,7 @@ class OptimizedImageField(ImageField):
     def deconstruct(self):
         name, path, args, kwargs = super().deconstruct()
 
-        # we just remove the arguments from the kwargs dict.
+        # Remove the arguments from the kwargs dict.
         kwargs.pop('optimized_image_output_size', None)
         kwargs.pop('optimized_image_resize_method', None)
         kwargs.pop('optimized_file_formats', None)
@@ -38,21 +35,7 @@ class OptimizedImageField(ImageField):
     def pre_save(self, model_instance, add):
         file = super().pre_save(model_instance, add)
 
-        if self._image_format == 'PNG':
-            cmd_args = ['cwebp', '-quiet', '-lossless', file.path, '-o', '-']
-        elif self._image_format == 'JPEG':
-            cmd_args = ['cwebp', '-quiet', file.path, '-o', '-']
-        elif self._image_format == 'GIF':
-            cmd_args = ['gif2webp', '-quiet', file.path, '-mixed', '-o', '-']
-        else:
-            return file
-
-        try:
-            webp_path = f'{os.path.splitext(file.path)[0]}.webp'
-            bytes_output = subprocess.check_output(cmd_args, timeout=30)
-            file.storage.save(webp_path, ContentFile(bytes_output))
-        except (subprocess.CalledProcessError, subprocess.TimeoutExpired):
-            pass
+        generate_webp(image_field=file.image)
 
         return file
 
@@ -74,16 +57,15 @@ class OptimizedImageField(ImageField):
         """Optimize an image that has not been saved to a file."""
         img = Image.open(image_data)
 
-        self._image_format = img.format
-        if self._image_format not in self.optimized_file_formats:
+        if img.format not in self.optimized_file_formats:
             raise ValidationError({self.name: ['Formato de imagen no soportado']})
 
         # GIF files needs strict size validation
-        if self._image_format == 'GIF' and output_size and output_size != (img.width, img.height):
+        if img.format == 'GIF' and output_size and output_size != (img.width, img.height):
             raise ValidationError({self.name: ['Tamaño de imagen GIF no soportado']})
 
         # Check if is a supported format for optimization
-        if self._image_format not in ['JPEG', 'PNG']:
+        if img.format not in ['JPEG', 'PNG']:
             return image_data
 
         # If output_size is set, resize the image with the selected resize_method.
@@ -93,11 +75,11 @@ class OptimizedImageField(ImageField):
             output_image = img
 
         # If the file extension is JPEG, convert the output_image to RGB
-        if self._image_format == 'JPEG':
+        if img.format == 'JPEG':
             output_image = output_image.convert('RGB')
 
         bytes_io = BytesIO()
-        output_image.save(bytes_io, format=self._image_format, optimize=True)
+        output_image.save(bytes_io, format=img.format, optimize=True)
 
         image_data.seek(0)
         image_data.file.write(bytes_io.getvalue())
---
+++ ax3_model_extras/webp.py
@@ -0,0 +1,28 @@
+import os.path
+import subprocess
+
+from django.core.files.base import ContentFile
+
+from PIL import Image
+
+
+def generate_webp(image_field):
+    """Generates a webp file. Supports PNG, JPEG and GIF files. If filepath is
+    `uploads/home/sunset.png` the generated file will be `uploads/home/sunset.webp`.
+    """
+    img = Image.open(image_field)
+    if img.format == 'PNG':
+        cmd_args = ['cwebp', '-quiet', '-lossless', image_field.path, '-o', '-']
+    elif img.format == 'JPEG':
+        cmd_args = ['cwebp', '-quiet', image_field.path, '-o', '-']
+    elif img.format == 'GIF':
+        cmd_args = ['gif2webp', '-quiet', image_field.path, '-mixed', '-o', '-']
+    else:
+        return
+
+    try:
+        webp_path = f'{os.path.splitext(image_field.path)[0]}.webp'
+        bytes_output = subprocess.check_output(cmd_args, timeout=30)
+        image_field.storage.save(webp_path, ContentFile(bytes_output))
+    except (subprocess.CalledProcessError, subprocess.TimeoutExpired):
+        pass
